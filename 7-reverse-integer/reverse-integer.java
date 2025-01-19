ef5536348828acaf42e79ec65000dc8117f652a0
Xopl2 <quentinlehman3366@gmail.com>
2025-01-18 20:42:39 -0600

    Time: 1 ms (84.48%) | Memory: 41.3 MB (15.23%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+
+        boolean isNegative = x < 0;
+        if(isNegative){
+            if(x == Integer.MIN_VALUE){
+                return 0;
+            }
+            x = -x;
+        }
+
+        while(x != 0){
+            int digit = x % 10;
+            x = x / 10;
+
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0; 
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        
+
+        if (isNegative) {
+            return -reversed;
+        } else {
+            return reversed;
+        }
+    }
+}
