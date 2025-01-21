bab303992001c694abd9ea1c1b6fd958063473d4
Quentin Lehman <quentinlehman3366@gmail.com>
2025-01-21 13:57:50 -0600

    Time: 0 ms (100.00%) | Memory: 42.6 MB (58.12%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -13,20 +13,18 @@ class Solution {
         ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
 
-        while(list1 != null && list2 != null){
 
+        while(list1 != null && list2 != null){
             if(list1.val < list2.val){
                 current.next = list1;
                 list1 = list1.next;
             }
-
             else{
                 current.next = list2;
                 list2 = list2.next;
             }
 
             current = current.next;
-
         }
 
         if(list1 != null){
@@ -37,5 +35,6 @@ class Solution {
         }
 
         return dummy.next;
+
     }
 }
